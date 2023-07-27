61bc1299fe1bcf794f314889babbbf61150d0f41
deepti <mdeepti3103@gmail.com>
2023-07-27 09:26:14 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,7 @@
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/.", methods = ["GET"])
+def root():
+    return "welcome to python flask app"
+app.run(host="0.0.0.0", port=4000, debug=True)
