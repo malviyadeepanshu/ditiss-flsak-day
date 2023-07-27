0cff140ff588aad6fa99ec1c10c3782aaac134cc
deepti <mdeepti3103@gmail.com>
2023-07-27 18:26:05 +0530

    new

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python
 WORKDIR /src
 RUN pip install flask
 COPY . .
-EXPOSE 4000
-CMD python
+EXPOSE 5000
+CMD python server.py
--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/", methods = ["GET"])
+
 def root():
-    return "welcome to python flask app"
-app.run(host="0.0.0.0", port=4000, debug=True)
+    return "welcome to my app"
+
+app.run(host="0.0.0.0", port=5000, debug=True)
